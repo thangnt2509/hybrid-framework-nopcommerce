9710d1a947791658242bd2ba02bd1eae8d32b997
thanngnt <thangnt6962@co-well.com.vn>
2022-06-20 18:42:44 +0700

    Attach image 1

---
+++ actions/reportConfig/ReportNGListener.java
@@ -0,0 +1,51 @@
+package reportConfig;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ReportNGListener implements ITestListener {
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
