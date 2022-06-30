7990afb12b0bd06a14d524827aa2e70b3964b6db
Hung Pham <hungpv@smartosc.com>
2022-06-30 22:57:41 +0700

    Fix loi run bat

--- RunAllure.bat
+++ RunAllure.bat
@@ -2,5 +2,5 @@ set ProjectPath=%~dp0
 cd %ProjectPath%
 echo %ProjectPath%
 set p=%PATH%
-java -javaagent:"%ProjectPath%libAllureReport\aspectjweaver-1.8.10.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllureReport\*;%ProjectPath%libAllureReport\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4J\*;%ProjectPath%libWebDriverManager\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceAdmin.xml"
+java -javaagent:"%ProjectPath%libAllure\aspectjweaver-1.8.10.jar" -classpath "%ProjectPath%bin;%ProjectPath%libAllure\*;%ProjectPath%libAllure\*;%ProjectPath%libExtentV5\*;%ProjectPath%libLog4J\*;%ProjectPath%libWebDriverManager\*;%ProjectPath%libReportNG\*;%ProjectPath%libSelenium\*" org.testng.TestNG "%ProjectPath%bin\runNopCommerceAdmin.xml"
 pause
